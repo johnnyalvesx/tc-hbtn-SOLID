712e9851bf6bcdb9bd30ee3c26bdbcf13df811b8
johnnyalvesx <alvesjohnny@outlook.com>
2022-04-11 18:17:01 -0300

    Convenções de nomenclatura

---
+++ 0x04/convencao/User.java
@@ -0,0 +1,17 @@
+public class User {
+
+    // classe users, representando nossa entidade
+
+    class UsersModels {
+
+        int idUser;
+
+        String descricao;
+
+        final static int IDADE_MINIMA = 18;
+
+// função que coleta todos os users
+
+        public getAllUsers() {
+            return [];
+        }
