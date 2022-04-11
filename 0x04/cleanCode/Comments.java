71648bb63a6833496864d405e2ea9c5147a3b3ca
johnnya1ves <johnnyalves@ciandt.com>
2022-04-11 17:51:29 -0300

    Comentários úteis

---
+++ 0x04/cleanCode/Comments.java
@@ -0,0 +1,22 @@
+public class Comments {
+
+    // Copyright (C) 2022 by CleanCode.
+    // Released under the terms of the GNU General Public License version 2 or later.
+
+    //public void checkXMLParser(){
+    //   // some business logic here
+    //}
+
+    /**
+     * <p> this method publishes results based on date
+     * </p>
+     *
+     * @param date date from when results should be published
+     * @return List of Result entities
+     * @since 16.0
+     */
+
+    public List<Result> publishResults(Date date) {
+        // some business logic here
+    }
+}
